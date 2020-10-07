d3f7489b99260ec0118bc93c8bf5976cc05bc886
Vanderlei Sbaraini Amancio <hello@vanderleis.me>
2020-10-07 19:27:14 -0300

    Slug field to be autogenerated automatically only on create pages

--- src/Support/Schema/Fields/SlugField.php
+++ src/Support/Schema/Fields/SlugField.php
@@ -11,22 +11,44 @@ class SlugField extends StringField implements ExportsToApiAttributeContract
     use ExportsToApiAttribute;
 
     protected string $baseField = '';
+    protected bool $makeFromFieldOnUpdate = false;
 
     public function getType(): string
     {
         return 'slug';
     }
 
-    public function makeFromField(string $field): self
+    public function makeFromField(string $field, bool $enableOnUpdate = false): self
     {
         $this->baseField = $field;
+        $this->makeFromFieldOnUpdate = $enableOnUpdate;
         return $this;
     }
 
     protected function resolveConfig(EntryInstanceContract $entryInstance, string $destination): void
     {
         $this->resolvedConfig = $this->resolvedConfig->merge([
-            'baseField' => $this->baseField,
+            'baseField' => $this->getBaseField($destination, $entryInstance),
         ]);
+    }
+
+    /**
+     * @param string                $destination
+     * @param EntryInstanceContract $entryInstance
+     * @return string|null
+     */
+    protected function getBaseField(string $destination, EntryInstanceContract $entryInstance): ?string
+    {
+        if ($destination === 'index') {
+            return null;
+        }
+
+        if (! $entryInstance->model()->exists) {
+            return $this->baseField;
+        }
+
+        return ($this->makeFromFieldOnUpdate)
+            ? $this->baseField
+            : null;
     }
 }
