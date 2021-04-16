c3bec3709f9a99ebbe5f486ece727117fe65cf6f
Vanderlei Sbaraini Amancio <hello@vanderleis.me>
2021-04-16 08:15:45 -0300

    Fixed media manager

--- src/EntryTypes/FileManager/WhereMimetype.php
+++ src/EntryTypes/FileManager/WhereMimetype.php
@@ -5,28 +5,58 @@ namespace CbtechLtd\Fastlane\EntryTypes\FileManager;
 use CbtechLtd\Fastlane\EntryTypes\QueryBuilder;
 use CbtechLtd\Fastlane\QueryFilter\Pipes\QueryPipeContract;
 use Closure;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
 
 class WhereMimetype implements QueryPipeContract
 {
     private array $mimetypes;
+    private bool $includeDirectories;
 
-    public function __construct(array $mimetypes)
+    /**
+     * WhereMimetype constructor.
+     * @param array $mimetypes
+     * @param bool $includeDirectories
+     */
+    public function __construct(array $mimetypes, bool $includeDirectories = true)
     {
         $this->mimetypes = $mimetypes;
+        $this->includeDirectories = $includeDirectories;
     }
 
     public function handle(QueryBuilder $query, Closure $next)
     {
-        foreach ($this->mimetypes as $type) {
-            if (Str::endsWith($type, '/*')) {
-                $query->getBuilder()->orWhere('mimetype', 'like', Str::replaceLast('/*', '/', $type) . '%');
-                continue;
+        $query->getBuilder()->where(function ($q) {
+            $queryTypes = $this->getMimetypeQueries();
+
+            $queryTypes->get('like', Collection::make())->each(
+                fn ($str) =>$q->orWhere('mimetype', 'like', $str)
+            );
+
+            if ($queryTypes->has('in')) {
+                $q->orWhereIn('mimetype', $queryTypes->get('in'));
             }
 
-            $query->getBuilder()->orWhere('mimetype', $type);
-        }
+            if ($this->includeDirectories) {
+                $q->orWhere('mimetype', 'fastlane/directory');
+            }
+        });
 
         return $next($query);
+    }
+
+    protected function getMimetypeQueries(): Collection
+    {
+        return Collection::make($this->mimetypes)->mapToGroups(function ($type) {
+            if (Str::endsWith($type, '/*')) {
+                return [
+                    'like' => Str::replaceLast('/*', '/', $type) . '%',
+                ];
+            }
+
+            return [
+                'in' => $type,
+            ];
+        });
     }
 }
